c2285e1420201c44aa2a050146599c99c2ef89d6
Andrew Keller <andrew@wimpyprogrammer.com>
2015-01-28 01:05:19 -0500

    Updating Tutorial to pass along the tour parameter to onShow() callbacks.

--- js/tutorial.js
+++ js/tutorial.js
@@ -21,8 +21,8 @@ define(function(require, exports, module) {
           orphan: true,
           content: 'Welcome to the Conditional Expression Parser.<br><br>Use your keyboard or the buttons below to navigate this brief tutorial.'
         },{
-          onShow: function() {
-            if(_options.onShow) { _options.onShow(); }
+          onShow: function(tour) {
+            if(_options.onShow) { _options.onShow(tour); }
             resetInput();
           },
           element: '.js-tutorial-input',
@@ -30,8 +30,8 @@ define(function(require, exports, module) {
           title: 'The Input Box',
           content: 'This textbox is where you\'ll enter the conditional expression you want to analyze.'
         },{
-          onShow: function() {
-            if(_options.onShow) { _options.onShow(); }
+          onShow: function(tour) {
+            if(_options.onShow) { _options.onShow(tour); }
             resetInput().val(example1);
           },
           element: '.js-tutorial-input',
@@ -39,8 +39,8 @@ define(function(require, exports, module) {
           title: 'A Simple Input',
           content: 'Let\'s begin with a simple example: two conditions, A and B, separated by an OR operator.<br><br>Notice that some syntax like the "if" statement and the opening bracket are included. They are unnecessary and will be ignored.'
         },{
-          onShow: function() {
-            if(_options.onShow) { _options.onShow(); }
+          onShow: function(tour) {
+            if(_options.onShow) { _options.onShow(tour); }
             resetInput().val(example1).change();
           },
           element: '.js-truth-table',
@@ -48,8 +48,8 @@ define(function(require, exports, module) {
           title: 'A Simple Output',
           content: 'The parser will generate a "truth table" showing all of the code paths in which the expression will evaluate to TRUE.  Our simple example has simple results.'
         },{
-          onShow: function() {
-            if(_options.onShow) { _options.onShow(); }
+          onShow: function(tour) {
+            if(_options.onShow) { _options.onShow(tour); }
             resetInput().val(example2);
           },
           element: '.js-tutorial-input',
@@ -57,8 +57,8 @@ define(function(require, exports, module) {
           title: 'A Complex Input',
           content: 'If your conditional expressions were so simple, you wouldn\'t need this tool!<br><br>Let\'s try something harder with sub-expressions, function calls, strings, and varied operators.'
         },{
-          onShow: function() {
-            if(_options.onShow) { _options.onShow(); }
+          onShow: function(tour) {
+            if(_options.onShow) { _options.onShow(tour); }
             resetInput().val(example2).change();
           },
           element: '.js-truth-table',
@@ -66,8 +66,8 @@ define(function(require, exports, module) {
           title: 'A Complex Output',
           content: 'Now our truth table is much larger. The different colored columns are used to group conditions within the same expression depth.<br><br>Notice that function parameters and strings are grouped into a single expression even if they look like conditional expressions.'
         },{
-          onShow: function() {
-            if(_options.onShow) { _options.onShow(); }
+          onShow: function(tour) {
+            if(_options.onShow) { _options.onShow(tour); }
             resetInput().val(example3);
           },
           element: '.js-tutorial-input',
@@ -75,8 +75,8 @@ define(function(require, exports, module) {
           title: 'The XOR Operator',
           content: 'Lastly we\'ll look at the XOR operator. Unlike the OR operator, the XOR operator requires that one or more conditions is FALSE for the overall expression to be TRUE.'
         },{
-          onShow: function() {
-            if(_options.onShow) { _options.onShow(); }
+          onShow: function(tour) {
+            if(_options.onShow) { _options.onShow(tour); }
             resetInput().val(example3).change();
           },
           element: '.js-truth-table',
